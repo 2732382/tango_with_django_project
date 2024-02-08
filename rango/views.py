a3746002ec9963ab6f7fa18c57236ad0f995e280
Ben Whitehead <2732382W@student.gla.ac.uk>
2024-02-08 22:28:19 +0000

    Ch9 completion - spelling errors mostly

--- rango/views.py
+++ rango/views.py
@@ -39,6 +39,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -53,6 +54,7 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form':form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -101,7 +103,7 @@ def register(request):
 
             profile.save()
 
-            reigstered = True
+            registered = True
         else:
             print(user_form.errors, profile_form.errors)
     else:
@@ -140,4 +142,4 @@ def user_logout(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
